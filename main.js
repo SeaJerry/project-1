ec867d7bfd0f80c12a5a57dac599bfffdd8361d8
Jerry_Sea <Jsea@kindsys.us>
2022-01-25 14:56:59 -0800

    Nav Functionality Working

--- main.js
+++ main.js
@@ -1,15 +1,8 @@
 
-// const $menuToggle = $('.toggle')
-
-// $menuToggle.addEventListener("click", () => {
-//     $menuToggle.toggleClass('active');
-// })
-
-
 const menuToggle = document.querySelector('.toggle');
-const showcase = document.querySelector('.menu');
+const menuShowcase= document.querySelector('.menu');
 
 menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('active');
-  showcase.classList.toggle('active');
+  menuShowcase.classList.toggle('active');
 })
