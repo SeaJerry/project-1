c2379c469cf76453537fa6fdff46157eac2d939e
Jerry_Sea <Jsea@kindsys.us>
2022-01-27 09:06:12 -0800

    adding carousel

--- main.js
+++ main.js
@@ -1,8 +1,40 @@
+// N A V B A R  &  M E N U  F U N C T I O N A L I T Y
 
-const menuToggle = document.querySelector('.toggle');
-const menuShowcase= document.querySelector('.menu');
 
-menuToggle.addEventListener('click', () => {
-  menuToggle.classList.toggle('active');
-  menuShowcase.classList.toggle('active');
-})
+
+$(document).ready(function (){
+    $('.next').on('click', function(){
+        const currentImg = $('.active')
+        const nextImg = currentImg.next()
+
+        if (nextImg.length){
+            currentImg.removeClass('active').css('z-index', -10);
+            nextImg.addClass('active').css('z-index', 10);
+        }
+    });
+
+    $('.previous').on('click', function(){
+        const currentImg = $('.active')
+        const previousImg = currentImg.prev()
+
+        if (previousImg.length){
+            currentImg.removeClass('active').css('z-index', -10);
+            previousImg.addClass('active').css('z-index', 10);
+        }
+    });
+});
+
+$(() => {
+
+    const $menuToggle = $('.toggle');
+    
+    const $menuShowcase = $('.menu');
+    
+    $menuToggle.click(function (){
+        $menuToggle.toggleClass('active');
+        $menuShowcase.toggleClass('active');
+    })
+});
+
+
+
